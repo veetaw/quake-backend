e3878ef7b9a61e6b091c22c732e6392c65b38a1d
alecsferra <aferrarini01@gmail.com>
2019-09-22 11:10:37 +0000

    defined quake schema

---
+++ core/src/quake_model.ts
@@ -0,0 +1,49 @@
+import { prop, Typegoose } from "typegoose";
+
+export class QuakeEntity extends Typegoose {
+    @prop()
+    public readonly eventName: string;
+    @prop()
+    public readonly author: string;
+    @prop()
+    public readonly creationTime: Date;
+    @prop()
+    public readonly coordinates: Coordinates;
+    @prop()
+    public readonly depth: number;
+    @prop()
+    public readonly depthUncertainity: number;
+    @prop()
+    public readonly magnitude: number;
+    @prop()
+    public readonly magnitudeUncertainity: number;
+    @prop()
+    public readonly stationCount: number;
+    @prop()
+    public readonly time: Date;
+
+    constructor(eventName: string,
+                author: string,
+                creationTime: Date,
+                coordinates: Coordinates,
+                depth: number,
+                depthUncertainity: number,
+                magnitude: number,
+                magnitudeUncertainity: number,
+                stationCount: number,
+                time: Date) {
+                super();
+                this.eventName = eventName;
+                this.author = author;
+                this.creationTime = creationTime;
+                this.coordinates = coordinates;
+                this.depth = depth;
+                this.depthUncertainity = depthUncertainity;
+                this.magnitude = magnitude;
+                this.magnitudeUncertainity = magnitudeUncertainity;
+                this.stationCount = stationCount;
+                this.time = time;
+        }
+}
+
+export const QuakeModel = new Typegoose().getModelForClass(QuakeEntity);
